0c824f61a7eda985d42373be71be3b7d5d23799d
ygilm <ygilm@bk.ru>
2023-06-03 01:49:06 +0400

    Added new file with new class

---
+++ src/video.py
@@ -0,0 +1,8 @@
+class Video:
+    def __init__(self, video_id, title, link, views, likes):
+        self.video_id = video_id
+        self.title = title
+        self.link = link
+        self.views = views
+        self.likes = likes
+
